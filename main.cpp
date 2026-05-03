4b4555871761deceef8dd6276c763d0e37a942bb
Yash <yashkumarshukla05@gmail.com>
2026-05-04 02:54:08 +0530

    Create main.cpp
    Added user input functionality to read two numbers.

---
+++ main.cpp
@@ -0,0 +1,45 @@
+# include <iostream> 
+using namespace std;
+
+int main(){
+    // cout<<"Hello World"<<endl;
+    // cout<<"Next Line";
+    // int a, b, c;
+    // short _sa = 9 ;
+    // cout<<_sa;
+    
+//camelacase Notation
+    // int marksInMaths = 10 ;
+    // cout<<"Marks of the student in maths -"<<marksInMaths;
+    
+    // string yash = "Yash";
+    // cout<<yash;
+    
+    // short a;
+    // int b;
+    // long c;
+    // long long d;
+// "const" does not allow to change the initialisation of variables
+    // float const score = 100.56; 
+    // double score2;
+    // double long score3 ;
+    // cout<<"The score is - "<<score;
+
+    
+    
+//user input
+    int a, b;
+    cout<<"Enter the first number : ";
+    cin>>a;
+    
+    cout<<"Enter the second number : ";
+    cin>>b;
+    
+    cout<<"The first number by the user is : "<<a<<endl;
+    cout<<"The second number by the user is : "<<b;
+    
+    
+
+    return 0;
+}
+
